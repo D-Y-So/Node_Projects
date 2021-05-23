e0e796cab7eaa508941d381c3da1b02260d8ec82
D-Y-So <68039134+D-Y-So@users.noreply.github.com>
2021-05-23 17:06:13 +0300

    add npm colors dependency

--- Day12/ex1-js/main.js
+++ Day12/ex1-js/main.js
@@ -1,3 +1,5 @@
+var colors = require('colors');
+
 console.log('Hello World');
 
 function Calculator(a,b){
@@ -18,9 +20,21 @@ Calculator.prototype.multiply = function(){
 }
 
 Calculator.prototype.log = function(){
-    console.log('',this.a +' + '+ this.b +' = ' + this.add(),'\n',
+    /*console.log('',this.a +' + '+ this.b +' = ' + this.add(),'\n',
                    this.a +' - '+ this.b +' = ' + this.subtract(),'\n',
                    this.a +' * '+ this.b +' = ' + this.multiply());
+                   */
+    var s1 = this.a +' + '+ this.b +' = ' + this.add();
+    s1 = s1.bgMagenta;
+    console.log(s1);
+
+    var s2 = this.a +' - '+ this.b +' = ' + this.subtract();
+    s2 = s2.rainbow;
+    console.log(s2);
+
+    var s3 = this.a +' - '+ this.b +' = ' + this.subtract();
+    s3 = s3.cyan;
+    console.log(s3);
 }
 
 var c1 = new Calculator(5,10);
@@ -30,8 +44,8 @@ var c2 = new Calculator(-3,27);
 c2.log();
 
 setTimeout(function(){c2.log()}, 2000);
+//setTimeout(c2.log.bind(c2),2000);
 
-setTimeout(c2.log.bind(c2),2000);
 
 //setTimeout(c2.log(), 2000); //calls the function immediatley //in node.js gives invalid callback error
 //setTimeout(c.log, 2000); //error
